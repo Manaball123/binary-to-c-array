e1e96cd5fcc56a3ac6e0f9c125d07b8949371f14
Mana <57663038+Manaball123@users.noreply.github.com>
2022-12-11 22:08:35 +0800

    major optimizations cuz this shit does NOT scale well

--- bin-to-c-arr.py
+++ bin-to-c-arr.py
@@ -3,15 +3,29 @@ import os
 
 
 elements_per_line = 16
-bytes_per_element = 4
+bytes_per_element = 8
 chars_per_element = bytes_per_element * 2
+#unit is element NOT byte
+null_padding_length = 131072
 append_name = "in.bin"
 out_name = "out.txt"
 
 out_buffer = ""
-append_buffer = "\n{"
+append_buffer = ""
 in_buffer = ""
 
+arr_type = ""
+if(bytes_per_element == 1):
+    arr_type = "unsigned char"
+elif(bytes_per_element == 2):
+    arr_type = "unsigned short"
+elif(bytes_per_element == 4):
+    arr_type = "unsigned int"
+elif(bytes_per_element == 8):
+    arr_type = "unsigned __int64"
+
+append_buffer += arr_type + " PAYLOAD[] = "
+append_buffer += "{"
 dir_items = os.listdir()
 for v in dir_items:
     if(v.find(".exe") != -1):
@@ -21,31 +35,54 @@ for v in dir_items:
 with open(append_name, "rb") as app_f:
     in_buffer = app_f.read()
 
-arr_str_unparsed = binascii.hexlify(in_buffer)
 
-if(arr_str_unparsed % chars_per_element != 0):
-    arr_str_unparsed += "0" * (chars_per_element - arr_str_unparsed % chars_per_element)
+#make sure the buffer is full
+if(len(in_buffer) % bytes_per_element != 0):
+    in_buffer += b"\x00" * (bytes_per_element - len(in_buffer) % bytes_per_element)
 
-for i in range(int(len(arr_str_unparsed) / chars_per_element)):
-    if(i % elements_per_line == 0):
-        append_buffer += "\n"
-    start_index = i * chars_per_element
+
+#should be bytes_per_element in length
+def reverse_endianness(bytes_arr : bytes):
     #reverse endianness
-    bytes_block_raw = []
-    for j in range(bytes_per_element):
-        index = start_index + j * 2
-        bytes_block_raw.append(bytes.decode(arr_str_unparsed[index:index + 2]))
+    bytes_block = bytearray([0] * 8)
 
-    bytes_block = ""
     for j in range(bytes_per_element):
-        bytes_block += bytes_block_raw[bytes_per_element - j - 1]
-    append_buffer += "0x" + bytes_block + ","
+        bytes_block[j] = bytes_arr[bytes_per_element - j - 1]
+    return bytes_block
+
+def get_formatted_element(bytes_block : bytes):
+    return "0x" + bytes.decode(binascii.hexlify(bytes_block)) + ","
+
+#index that handles the indentation
+g_index = 0
+
+elements_len = int(len(in_buffer) / bytes_per_element)
+
+
+#element at end may require padding
+for i in range(elements_len - 1):
+    if(g_index % elements_per_line == 0):
+        append_buffer += "\n"
+    start_index = i * bytes_per_element
+    bytes_arr = in_buffer[start_index:start_index + bytes_per_element]
     
- 
+    append_buffer += get_formatted_element(reverse_endianness(bytes_arr))
+    g_index += 1
 
 
 
-append_buffer += "\n}"
+EMPTY_ELEMENT = get_formatted_element(b"\x00" * bytes_per_element)
+for i in range(null_padding_length):
+    if(g_index % elements_per_line == 0):
+        append_buffer += "\n"
+    append_buffer += EMPTY_ELEMENT
+    g_index += 1
+
+
+
+
+
+append_buffer += "\n};"
 out_buffer += append_buffer
 with open(out_name,"w") as out:
     out.write(out_buffer)
